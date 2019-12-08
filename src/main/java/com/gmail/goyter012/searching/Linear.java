68a8063e276c3db31760b4d9c28b43a8af4e9a57
G0yter <goyter012@gmail.com>
2019-12-08 18:38:09 +0200

    quicksort added

--- src/main/java/com/gmail/goyter012/searching/Linear.java
+++ src/main/java/com/gmail/goyter012/searching/Linear.java
@@ -2,7 +2,7 @@ package com.gmail.goyter012.searching;
 
 public class Linear {
 
-    public static int  linearSearch(int[] arr, int toFind){
+    public static int linearSearch(int[] arr, int toFind){
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == toFind){
                 return i;
